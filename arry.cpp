9bd13cfd506e743607577cb86e209fb7b45fd861
Amikshaindurkar <104967813+Amikshaindurkar@users.noreply.github.com>
2022-05-11 13:15:44 +0530

    Add files via upload

---
+++ arry.cpp
@@ -0,0 +1,16 @@
+//write a program to find sum of n arry element
+//roll no-ME21D035
+#include<stdio.h>
+int main()
+{
+	int nbr,i,r,arr[10];
+	printf("Enter the number of elements in the array:");
+	scanf("%d",&nbr);
+	printf("Enter the array elements:");
+	for (i=0;i<nbr;i++)
+	printf("Enter the item to be searched: ");
+	scanf("%d",&r);
+	if (i<nbr);
+	else
+	return 0;
+}
